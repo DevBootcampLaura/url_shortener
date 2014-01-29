afc4f75607f0961a4215f925fea093da7d30580a
Apprentice <apprentice@dbc-chicago-6.local>
2014-01-29 10:38:18 -0600

    finished product mvp

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,22 +1,18 @@
 get '/' do
-  # let user create new short URL, display a list of shortened URLs
-
   erb :index
 end
 
 post '/urls' do
   input = params[:long_url]
-  @data = Url.create(long_url: input, click_count: 0)
+  @data = Url.create!(long_url: input, click_count: 0)
   erb :show_url
 end
 
-# e.g., /q6bda
+
 get '/:short_url' do
-  # redirect to appropriate "long" URL
   params[:short_url]
   @data = Url.find_by(short_url: params[:short_url])
   @data.click_count += 1
   @data.save
-  # increment counter for that URL
   redirect to("#{@data.long_url}")
 end
